91c4c71a5c2f1e1c762e6ed753382b295e3e7af1
Shivasunder20 <shivasunderjaligapu@gmail.com>
2023-01-29 16:43:04 +0530

    Sql_assignments

--- 16jan.sql
+++ 16jan.sql
@@ -2,4 +2,15 @@ create table second_highest(emp char,deptno int,salary int);
 insert into second_highest values('A',10,1000),('B',10,2000),('C',10,3000)
 ,('D',20,7000),('E',20,9000),('F',20,8000),('G',30,17000),
 ('H',30,15000),('I',30,30000);
+
+
 SELECT * FROM second_highest;
+
+
+select emp as EName,deptno as deptNo,salary FROM (
+SELECT emp,deptno,salary,
+row_number() over w as row_num
+from second_highest
+window w as(partition by deptno order by salary desc))
+e
+where row_num=2;
